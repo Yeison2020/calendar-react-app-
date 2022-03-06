4b294bf854c1805dc0462f823d946ee9126a556f
Yeison <enmanuelquiterios@gmail.com>
2022-03-06 13:17:13 -0500

    Adding logout icon

--- src/Components/ui/NavBar.js
+++ src/Components/ui/NavBar.js
@@ -5,6 +5,7 @@ const NavBar = () => {
     <div className="navbar navbar-dark bg-dark mb-4">
       <span className="navbar-brand">Juan Lopez</span>
       <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt"></i>
         <span>Logout</span>
       </button>
     </div>
