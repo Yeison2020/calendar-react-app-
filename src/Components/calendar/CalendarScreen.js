d95924120c90483d86015fd2dbf3913a45ad185a
Yeison <enmanuelquiterios@gmail.com>
2022-03-06 13:39:39 -0500

    Adding calendar libray

--- src/Components/calendar/CalendarScreen.js
+++ src/Components/calendar/CalendarScreen.js
@@ -1,10 +1,27 @@
 import React from "react";
 import NavBar from "../ui/NavBar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+const localizer = momentLocalizer(moment);
+const events = [
+  {
+    title: "Learn React Today",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+  },
+];
 
 const CalendarScreen = () => {
   return (
     <div>
       <NavBar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccesor="start"
+        endAccesor="end"
+      />
     </div>
   );
 };
