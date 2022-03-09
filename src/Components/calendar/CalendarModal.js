963e42e65285ec3f1f960c5226a3604b67e5dcf8
Yeison <enmanuelquiterios@gmail.com>
2022-03-09 13:24:49 -0500

    Working modal

--- src/Components/calendar/CalendarModal.js
+++ src/Components/calendar/CalendarModal.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
 
 const CalendarModal = () => {
-  return <div>CalendarModel</div>;
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      // onAfterOpen={afterOpenModal}
+      // onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      <h1>Hello World</h1>
+      <hr />
+      <span>Hello Yeison</span>
+    </Modal>
+  );
 };
 
 export default CalendarModal;
